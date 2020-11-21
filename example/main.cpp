02cea15e52639ca85b877c9ae9a786810d19ffa4
poifull10 <poifull10@gmail.com>
2020-11-22 01:30:42 +0900

    Refactor example

--- example/main.cpp
+++ example/main.cpp
@@ -7,19 +7,15 @@
 int main(int argc, char** argv) {
   render::Camera camera(1001, 1001);
   render::Sphere sphere(3.F, render::Vec(0, 0, 10));
-
   std::cout << "rendering ... " << std::endl;
   for (auto h = 0; h < camera.height(); h++) {
     for (auto w = 0; w < camera.width(); w++) {
       const auto ray = camera.ray(w, h);
       if (sphere.hit(ray)) {
         camera.image(w, h) = render::RGB(255, 0, 255);
-      } else {
-        camera.image(w, h) = render::RGB(0, 0, 0);
       }
     }
   }
-
   camera.save("sphere.png");
   std::cout << "rendering is done! " << std::endl;
 }
